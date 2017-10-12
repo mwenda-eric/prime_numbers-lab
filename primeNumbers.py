dbd454430a1c8dfd4525f4ec9a2c9e929f027c3e
mwenda-eric <ericmwenda5@gmail.com>
2017-10-12 12:53:09 +0000

    [fearure] implemented a function to return a list of prime numbers

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,16 @@
-
+def primeNumbers(n):
+    """returns a list of prime numbers"""
+    primes = []
+    if type(n)!=int:
+        raise TypeError
+    elif n <=1: 
+        return primes
+    else:
+        for i in range(2,n+1):
+            isprime=True
+            for num in primes:
+                if(i%num==0):
+                    isprime=False
+            if(isprime):
+                primes.append(i)
+        return primes
